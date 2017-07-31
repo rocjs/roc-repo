6798f8729a4bcffa86463828cc233c31fa8dacd4
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2017-07-31 16:37:26 +0200

    fix(roc-plugin-repo): Use default .eslintignore file

--- extensions/roc-plugin-repo/src/commands/lint.js
+++ extensions/roc-plugin-repo/src/commands/lint.js
@@ -1,10 +1,12 @@
+import path from 'path';
+
 import { execute, fileExists } from 'roc';
 import log from 'roc/log/default/small';
 
 const eslint = require.resolve('eslint/bin/eslint');
 
-const eslintCommand = (project, fix, config, output) =>
-  `${eslint} ${config}${project.path} --ignore-pattern '${output}' ${fix
+const eslintCommand = (project, fix, config, output, ignoreFile) =>
+  `${eslint} ${config}${project.path} --ignore-path '${ignoreFile}' --ignore-pattern '${output}' ${fix
     ? '--fix'
     : ''}`;
 
@@ -32,10 +34,22 @@ export default projects => ({
     config = `--config ${require.resolve('../configuration/eslintrc.js')} `;
   }
 
+  let ignoreFile;
+  if (fileExists('.eslintignore', context.directory) && !forceDefault) {
+    log.info(
+      'A local ESLint ignore file was detected and will be used over the default.\n',
+    );
+    ignoreFile = path.join(context.directory, '.eslintignore');
+  } else {
+    ignoreFile = require.resolve('../configuration/eslintignore');
+  }
+
   return Promise.all(
     selected
       .map(project =>
-        execute(eslintCommand(project, fix, config, settings.output)),
+        execute(
+          eslintCommand(project, fix, config, settings.output, ignoreFile),
+        ),
       )
       .map(promise => promise.then(() => 0, () => 1)),
   ).then(results => {
